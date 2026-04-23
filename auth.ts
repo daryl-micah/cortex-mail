8bd1072dd0e94d56b114c8d166a88706997fcf16
Daryl Micah <darylmicah12@gmail.com>
2026-04-23 21:18:23 +0530

    fix: gmail OAuth scopes

--- auth.ts
+++ auth.ts
@@ -22,7 +22,6 @@ const config = {
             'openid',
             'https://www.googleapis.com/auth/userinfo.email',
             'https://www.googleapis.com/auth/userinfo.profile',
-            'https://www.googleapis.com/auth/gmail.readonly',
             'https://www.googleapis.com/auth/gmail.send',
             'https://www.googleapis.com/auth/gmail.modify',
           ].join(' '),
